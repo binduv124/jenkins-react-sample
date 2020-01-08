97dfc0adca114df54a572338c40c9c2564a5b169
Bindu <binduv124@gmail.com>
2020-01-08 15:21:22 +0530

    Add files via upload

---
+++ build/precache-manifest.727ad65bf30fe7596fc3e2fbc99cd525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9843728901bc9ca5cc749d8fcd7ba6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcffb64c6dd97441c771",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0692464967cc3113cfcc",
+    "url": "/static/js/2.ae1d6b93.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ae1d6b93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fcffb64c6dd97441c771",
+    "url": "/static/js/main.b8cf7907.chunk.js"
+  },
+  {
+    "revision": "831898f453b8da6d7e45",
+    "url": "/static/js/runtime-main.c65eda97.js"
+  }
+]);
